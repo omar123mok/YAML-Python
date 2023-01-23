07478f1f5b04033c509230c35c6e5808f1a4a1ef
omar123mok <106643874+omar123mok@users.noreply.github.com>
2023-01-23 18:57:51 +0530

    Create test_validation.py

---
+++ test_validation.py
@@ -0,0 +1,29 @@
+import yaml
+import pytest
+
+
+# pip install pyyaml
+def func1(data):
+    ''' Tests for the format of code'''
+    for item in data:
+        assert 'First Name' in item, f'Missing key: First Name in {item}'
+        assert 'Last Name' in item, f'Missing key: Last Name in {item}'
+        assert 'Gender' in item, f'Missing key: Gender in {item}'
+        assert 'Tags' in item, f'Missing key: Tags in {item}'
+        assert item['First Name'], f'Missing value for key: First Name in {item}'
+        assert item['Last Name'], f'Missing value for key: Last Name in {item}'
+        assert item['Gender'], f'Missing value for key: Gender in {item}'
+        assert item['Tags'], f'Missing value for key: Tags in {item}'
+
+
+def test_01():
+    ''' Reads the data.yaml file'''
+    with open("data.yaml", 'r') as x:
+        data = x
+    with pytest.raises(Exception) as exc:
+        data = yaml.safe_load(x)
+
+    with pytest.raises(Exception) as exc:
+        func1(data)
+    print(str(exc.value))
+    # assert (str(exc.value)) == "Value Error at func1"
