537a154a07899be2131f6fb366aed87504981a8c
omar123mok <106643874+omar123mok@users.noreply.github.com>
2023-01-24 13:39:06 +0530

    Update test_validation.py

--- test_validation.py
+++ test_validation.py
@@ -3,9 +3,11 @@ import pytest
 
 
  with open("data.yaml", 'r') as x:
-        data = yaml.safe_load(x)
-def test_func1():
+      data = yaml.safe_load(x)
    
+
+def test_func1(data):
+    print(data)
     for item in data:
         assert 'First Name' in item, f'Missing key: First Name in {item}'
         assert 'Last Name' in item, f'Missing key: Last Name in {item}'
@@ -15,3 +17,4 @@ def test_func1():
         assert item['Last Name'], f'Missing value for key: Last Name in {item}'
         assert item['Gender'], f'Missing value for key: Gender in {item}'
         assert item['Tags'], f'Missing value for key: Tags in {item}'
+
