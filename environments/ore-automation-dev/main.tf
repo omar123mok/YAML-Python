8bb544927dec1b51023c11bfc949b7be6459d28f
omar123mok <106643874+omar123mok@users.noreply.github.com>
2023-04-11 15:29:43 +0530

    create file

---
+++ environments/ore-automation-dev/main.tf
@@ -0,0 +1,24 @@
+locals {
+  environment = "ore-automation-dev"
+  workload    = "dev"
+  alias       = "31312323"
+  tags        = merge(local.common-tags, { env = "ore-automation-dev" })
+}
+
+module "iam" {
+  source                  = "../../modules/iam"
+  alias                   = local.alias
+  tags                    = local.tags
+  allow-manual-parameters = true
+  ec2-ssm-role            = true
+  cloudability-eid        = "31312323"
+}
+
+module "config" {
+  source = "../../modules/config"
+  tags   = local.tags
+}
+
+module "s3" {
+  source = "../../modules/s3"
+}
